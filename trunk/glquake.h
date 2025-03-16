1adc4c2ee20c6c0b3df8b090afb4414c20249866
Florian Piesche <florian@yellowkeycard.net>
2025-03-16 00:28:28 +0000

    Add another gl include for macos

--- trunk/glquake.h
+++ trunk/glquake.h
@@ -33,6 +33,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef USEFAKEGL
     #ifdef __APPLE__
         #include <OpenGL/gl.h>
+        #include <OpenGL/gl3.h>
         #include <OpenGL/glext.h>
     #else
         #include <GL/gl.h>
