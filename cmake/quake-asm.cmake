719b7a5e72a176cd82d23a37640fd9c6632c2a07
Florian Piesche <florian@yellowkeycard.net>
2025-03-18 23:19:50 +0000

    Try and use bundled gas2asm

--- cmake/quake-asm.cmake
+++ cmake/quake-asm.cmake
@@ -2,7 +2,7 @@ if (WIN32)
     foreach(file ${JQ_GASM})
         execute_process(COMMAND ${CMAKE_C_COMPILER} /nologo /EP ${file}
             OUTPUT_FILE ${file}.spp)
-        execute_process(COMMAND build/bin/gas2masm.exe
+        execute_process(COMMAND Windows/VisualStudio/gas2masm.exe
            INPUT_FILE ${file}.spp
            OUTPUT_FILE ${file}.asm)
         execute_process(COMMAND ml.exe /nologo /c /Cp /coff /Fo${file}.obj /Zm /Zi ${file}.asm)
