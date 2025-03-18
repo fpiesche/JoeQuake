0d9cd03ad0bf52190ec993581dcd5c60a0066647
Florian Piesche <florian@yellowkeycard.net>
2025-03-18 22:42:14 +0000

    donk

--- cmake/quake-asm.cmake
+++ cmake/quake-asm.cmake
@@ -1,7 +1,6 @@
 if (WIN32)
     foreach(file ${JQ_GASM})
-        execute_process(COMMAND ${CMAKE_C_COMPILER} /nologo /EP
-            INPUT_FILE ${file}
+        execute_process(COMMAND ${CMAKE_C_COMPILER} /nologo /EP ${file}
             OUTPUT_FILE ${file}.spp)
         execute_process(COMMAND build/bin/gas2masm.exe
            INPUT_FILE ${file}.spp
