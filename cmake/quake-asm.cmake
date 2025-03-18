d1eedc289eb0c0ab099fc7aa5b98eb2dda6f7fe0
Florian Piesche <florian@yellowkeycard.net>
2025-03-18 22:30:20 +0000

    Try and fix execute_process commands

--- cmake/quake-asm.cmake
+++ cmake/quake-asm.cmake
@@ -1,7 +1,11 @@
 if (WIN32)
     foreach(file ${JQ_GASM})
-        execute_process(COMMAND ${CMAKE_C_COMPILER} /nologo /EP > ${file}.spp ${file})
-        execute_process(COMMAND build/bin/gas2masm.exe < ${file}.spp > ${file}.asm)
+        execute_process(COMMAND ${CMAKE_C_COMPILER} /nologo /EP
+            INPUT_FILE ${file}
+            OUTPUT_FILE ${file}.spp)
+        execute_process(COMMAND build/bin/gas2masm.exe
+           INPUT_FILE ${file}.spp
+           OUTPUT_FILE ${file}.asm)
         execute_process(COMMAND ml.exe /nologo /c /Cp /coff /Fo${file}.obj /Zm /Zi ${file}.asm)
     endforeach()
 endif()
